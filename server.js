aae5342978e80b1b867cf6368842e71b888043ec
Yashraj0103 <official.yash0103@gmail.com>
2026-05-02 10:31:03 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-~require("dotenv").config();
+require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -9,34 +9,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Auth routes
-const authRoutes = require("./routes/auth");
-app.use("/api/auth", authRoutes);
-
-// Task routes
-const taskRoutes = require("./routes/Task");
-app.use("/api/tasks", taskRoutes);
-
-const dashboardRoutes = require("./routes/dashboard");
-app.use("/api/dashboard", dashboardRoutes);
-
-const projectRoutes = require("./routes/project");
-app.use("/api/projects", projectRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
-
-const cors = require("cors");
-app.use(cors());
-
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/tasks", require("./routes/task"));   // lowercase
+app.use("/api/dashboard", require("./routes/dashboard"));
+app.use("/api/projects", require("./routes/project"));
 app.use("/api/users", require("./routes/user"));
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
+// Root test
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000")); 
+// Connect DB then start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
+  })
+  .catch(err => console.log(err));
